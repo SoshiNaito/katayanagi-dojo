d6913b66196ba2acd18ad1a8157aa1e129836b58
rin1208 <sora1208sky@gmail.com>
2020-09-10 00:49:35 +0900

    add

--- front-end/src/components/PostList.js
+++ front-end/src/components/PostList.js
@@ -25,23 +25,6 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
-*/
 function PostList() {
     const classes = useStyles();
 
